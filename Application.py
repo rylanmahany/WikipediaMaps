94e2151f92e29f4df09527793139a60016ea8541
Rylan Mahany <rbm2526@g.rit.edu>
2023-10-19 15:31:56 -0400

    Made a lot of progress on the BFS and web crawler portions. Getting an error: raise RequestsSSLError(e) requests.exceptions.SSLError: [SSL: DECRYPTION_FAILED_OR_BAD_RECORD_MAC] decryption failed or bad record mac (_ssl.c:2633)
    which is something in the networking side from what I've seen online, not so good. Maybe it'll just work later, Wikipedia was getting hounded for a few hours.

--- Application.py
+++ Application.py
@@ -1,13 +1,19 @@
+import re
 from tkinter import *
 from PIL import ImageTk, Image
 from urllib.request import urlopen
+import validators
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
 
-
+#Main Function, Defines and Loops the GUI
 def main():
 #Define window
     window = Tk()
     window.title("WikipediaMaps")
-    window.geometry("500x500")
+    window.geometry("600x600")
 
     #Add photo to the top of window
     img = ImageTk.PhotoImage(Image.open("wikimaps.png").resize((100, 100)))
@@ -30,32 +36,74 @@ def main():
     destinationTextInput.grid(column=1, row=2)
 
     #Add output window
-    output = Label(window, text="Welcome to WikipediaMaps! Enter the link you want to start at in the first box, and the destination in the second. The path will be given to you here!")
+    output = Label(window, text="Welcome to WikipediaMaps! Enter the link you want to start at\nin the first box, and the destination in the second. The path will be given to you here!")
     output.grid(column=1, row=4)
 
-    #Define capture input function for the button
+    #Define capture input function. Captures Start and Destination Text. Will be called when pressing start! button.
     def captureInput():
         start = startTextInput.get()
         destination = destinationTextInput.get()
-        if urlopen(start).getcode() == 404 and urlopen(destination).getcode() == 404:
+        if not validators.url(start) and not validators.url(destination):
             output.config(text="Enter a valid Start and Destination Link")
-        elif urlopen(start).getcode() == 404:
+        elif not validators.url(start):
             output.config(text="Enter a valid Start Link")
-        elif urlopen(destination).getcode() == 404:
+        elif not validators.url(destination):
             output.config(text="Enter a valid Destination link")
         else:
+            output.config(text="Searching...")
+            time.sleep(2)
             output.config(text=findPath(start, destination))
 
 
     #Add the search button
     B = Button(text="Start!", command=captureInput)
     B.grid(column=1, row=3)
+
+    #Loop the GUI
     window.mainloop()
 
 
-
+#Takes the start and end websites and will find the path using BFS with the links on each page.
 def findPath(start, end):
-    return "Connection not found!"
+    queue = [start]
+    previous = {}
+    previous[start]: None
+    while queue is not None:
+        current_page = queue.pop(0)
+        if current_page is end:
+            return reconstructPath(previous, current_page)
+        neighbors = getNeighbors(start)
+        for n in neighbors:
+                queue.append(n)
+                previous[n] = current_page
+    return "No path was found!"
 
 
+#Function takes a dictionary of a path, and traces it all the way back to the start
+def reconstructPath(previous, current_page):
+    print("building path")
+    path = []
+    key = current_page
+    while previous[key] is not None:
+        path.append(previous[key])
+        key = previous[key]
+    return path
+
+
+#Takes the start website link and returns a list of all the URLs on the page
+def getNeighbors(start):
+    neighbors = []
+    filteredNeighbors = []
+    request = requests.get(start)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    neighbors = soup.find(id='bodyContent').select('a')
+    for link in neighbors:
+        if link.get('href') is not None:
+            if link.get('href').startswith("/wiki/"):
+                filteredNeighbors.append("https://en.wikipedia.org" + link.get('href'))
+    print("searched site: " + start + "\nFound: " + ",".join(str(l) for l in filteredNeighbors))
+    return filteredNeighbors
+
+
+#invocation of main function
 main()
